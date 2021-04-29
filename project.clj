fe14f1bfee962d83195b0ca5372bd5b34d403b5d
Paul Lam <paul@quantisan.com>
2021-04-29 09:14:13 +0900

    update all libraries

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject uwcpdx/stepwise "0.5.9-SNAPSHOT"
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/clojure "1.9.0"]
-                                  [org.clojure/spec.alpha "0.1.143"]
-                                  [pjstadig/humane-test-output "0.8.1"]
-                                  [org.slf4j/slf4j-simple "1.7.25"]
-                                  [com.gfredericks/test.chuck "0.2.7"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.1.0"]
+                                  [org.clojure/clojure "1.10.3"]
+                                  [org.clojure/spec.alpha "0.2.194"]
+                                  [pjstadig/humane-test-output "0.11.0"]
+                                  [org.slf4j/slf4j-simple "1.7.30"]
+                                  [com.gfredericks/test.chuck "0.2.10"]
                                   [org.clojure/clojure "1.9.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
-                                  [org.clojure/test.check "0.9.0"]]
+                                  [org.clojure/test.check "1.1.0"]]
                    :injections   [(require 'pjstadig.humane-test-output)
                                   (pjstadig.humane-test-output/activate!)]
                    :main         stepwise.dev-repl}}
@@ -16,11 +16,11 @@
                                    :username      :env/CLOJARS_USERNAME
                                    :password      :env/CLOJARS_PASSWORD
                                    :sign-releases false}}
-  :dependencies [[uwcpdx/bean-dip "0.7.5"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/core.async "0.4.474"]
-                 [com.amazonaws/aws-java-sdk-iam "1.11.230"]
-                 [com.amazonaws/aws-java-sdk-sts "1.11.230"]
-                 [com.amazonaws/aws-java-sdk-stepfunctions "1.11.230"]])
+  :dependencies [[uwcpdx/bean-dip "0.7.6"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/data.json "2.2.2"]
+                 [org.clojure/core.async "1.3.618"]
+                 [com.amazonaws/aws-java-sdk-iam "1.11.1007"]
+                 [com.amazonaws/aws-java-sdk-sts "1.11.1007"]
+                 [com.amazonaws/aws-java-sdk-stepfunctions "1.11.1007"]])
 
