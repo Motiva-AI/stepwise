553091f861ecded54f7556c3057926087178eaf0
Paul Lam <paul@quantisan.com>
2021-05-08 08:10:07 +0900

    update project.clj and add pipelien-transit library

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
                                   [pjstadig/humane-test-output "0.11.0"]
                                   [org.slf4j/slf4j-simple "1.7.30"]
                                   [com.gfredericks/test.chuck "0.2.10"]
-                                  [org.clojure/clojure "1.9.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
                                   [org.clojure/test.check "1.1.0"]]
@@ -22,6 +21,7 @@
                  [org.clojure/tools.logging "1.1.0"]
                  [org.clojure/data.json "2.2.2"]
                  [org.clojure/core.async "1.3.618"]
+                 [ai.motiva/pipeline-transit "0.1.0"]
                  [com.amazonaws/aws-java-sdk-iam "1.11.1007"]
                  [com.amazonaws/aws-java-sdk-sts "1.11.1007"]
                  [com.amazonaws/aws-java-sdk-stepfunctions "1.11.1007"]])
