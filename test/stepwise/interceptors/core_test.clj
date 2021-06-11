e74ca5e90cb85bd1e88282ce885dbd364c3963ff
Paul Lam <paul@quantisan.com>
2021-06-12 08:40:12 +0900

    add test case to ensure that send-heartbeat-fn is attached to context

--- test/stepwise/interceptors/core_test.clj
+++ test/stepwise/interceptors/core_test.clj
@@ -1,6 +1,6 @@
 (ns stepwise.interceptors.core-test
   (:refer-clojure :exclude [compile])
-  (:require [clojure.test :as test]
+  (:require [clojure.test :refer [deftest testing is]]
             [stepwise.interceptors.core :as main]))
 
 (def inc-x-interceptor-tuple
@@ -13,7 +13,13 @@
 (defn execute [queue task]
   ((main/compile queue handler) task (fn [])))
 
-(test/deftest compile-test
-  (test/is (= {:y 42}
-              (execute [inc-x-interceptor-tuple] {:x 40}))))
+(deftest compile-test
+  (testing "interceptor with handler-fn"
+    (is (= {:y 42}
+           (execute [inc-x-interceptor-tuple] {:x 40}))))
+
+  (testing "send-heartbeat-fn is associated to internal context"
+    (execute [[:check-heartbeat-fn
+               {:enter (fn [ctx] (is (fn? (:send-heartbeat-fn ctx))) ctx)}]]
+             {:x 40})))
 
