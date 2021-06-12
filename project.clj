e1acde262f8295c8878306b0b15f14bfb04f8528
Paul Lam <paul@quantisan.com>
2021-06-12 19:55:25 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ai.motiva/stepwise "1.0.0-SNAPSHOT"
+(defproject ai.motiva/stepwise "0.6.0-SNAPSHOT"
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.1.0"]
                                   [org.clojure/clojure "1.10.3"]
                                   [org.clojure/spec.alpha "0.2.194"]
                                   [pjstadig/humane-test-output "0.11.0"]
                                   [org.slf4j/slf4j-simple "1.7.30"]
-                                  [com.gfredericks/test.chuck "0.2.10"]
+                                  [com.gfredericks/test.chuck "0.2.11"]
                                   [circleci/bond "0.5.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
@@ -20,10 +20,10 @@
 
   :dependencies [[uwcpdx/bean-dip "0.7.6"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [org.clojure/data.json "2.2.2"]
+                 [org.clojure/data.json "2.3.1"]
                  [org.clojure/core.async "1.3.618"]
                  [metosin/sieppari "0.0.0-alpha13"]
-                 [com.amazonaws/aws-java-sdk-iam "1.11.1007"]
-                 [com.amazonaws/aws-java-sdk-sts "1.11.1007"]
-                 [com.amazonaws/aws-java-sdk-stepfunctions "1.11.1007"]])
+                 [com.amazonaws/aws-java-sdk-iam "1.12.5"]
+                 [com.amazonaws/aws-java-sdk-sts "1.12.5"]
+                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.5"]])
 
