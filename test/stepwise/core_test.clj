3c48a27875cde2986ecb41ee016629bc01449b0d
Paul Lam <paul@quantisan.com>
2021-10-19 12:00:35 +0900

    rename adder-state-machine-test assets for clarity

--- test/stepwise/core_test.clj
+++ test/stepwise/core_test.clj
@@ -5,22 +5,22 @@
 (def bucket-name "stepwise-integration-test")
 
 (deftest ^:integration adder-state-machine-test
-  (is (sw/ensure-state-machine :adder
+  (is (sw/ensure-state-machine :stepwise-integration-test
                                {:start-at :add
                                 :states   {:add {:type     :task
-                                                 :resource :activity/add
+                                                 :resource :stepwise-integration-test/add
                                                  :heartbeat-seconds 3
                                                  :end      true}}}))
 
   (let [workers (sw/start-workers!
-                  {:activity/add (fn [{:keys [x y]}] (+ x y)) })]
+                  {:stepwise-integration-test/add (fn [{:keys [x y]}] (+ x y)) })]
 
     (is (= {:output 2
             :status "SUCCEEDED"}
            (as-> {:x 1 :y 1} $
              (sw/offload-select-keys-to-s3 $ [:x :y] bucket-name)
              (hash-map :input $)
-             (sw/start-execution!! :adder $)
+             (sw/start-execution!! :stepwise-integration-test $)
              (select-keys $ [:output :status]))))
     (is (= #{:done} (sw/shutdown-workers workers)))))
 
