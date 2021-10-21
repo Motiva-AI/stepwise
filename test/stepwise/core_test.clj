a3583542cd000c964032716572d3710e03dd1295
Paul Lam <paul@quantisan.com>
2021-10-21 11:41:04 +0900

    core-test/manipulate-task-input-and-output-test pass

--- test/stepwise/core_test.clj
+++ test/stepwise/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [stepwise.core :as sw]
             [stepwise.arns :as arns]
-            [stepwise.client :as client]))
+            [stepwise.client :as client]
+            [clojure.data.json :as json]))
 
 (def state-machine-keyword-name :stepwise-integration-test)
 (def bucket-name "stepwise-integration-test")
@@ -21,6 +22,35 @@
       (is (= {:output 2
               :status "SUCCEEDED"}
              (as-> {:x 1 :y 1} $
+               (sw/offload-select-keys-to-s3 $ [:x :y] bucket-name)
+               (hash-map :input $)
+               (sw/start-execution!! :stepwise-integration-test $)
+               (select-keys $ [:output :status]))))
+      (is (= #{:done} (sw/shutdown-workers workers))))))
+
+(deftest ^:integration manipulate-task-input-and-output-test
+  (when (is (sw/ensure-state-machine state-machine-keyword-name
+                                     {:start-at :add
+                                      :states   {:add {:type     :task
+                                                       :resource :stepwise-integration-test/add
+                                                       :comment  "unit test task"
+                                                       :parameters (json/write-str {:comment "replaced comment"
+                                                                                    "MyInput" {"newX.$" "$.x"
+                                                                                               "newY.$" "$.y"}})
+                                                       :result-selector (json/write-str {"newZ.$" "$.z"})
+                                                       :result-path "$.add-output"
+                                                       :heartbeat-seconds 3
+                                                       :end      true}}}))
+
+    (let [workers (sw/start-workers!
+                    {:stepwise-integration-test/add (fn [{:keys [MyInput]}]
+                                                      (let [{x :newX
+                                                             y :newY} MyInput]
+                                                        {:z (+ x y)}))})
+          input   {:x 1 :y 1}]
+      (is (= {:output (merge input {:add-output {:newZ 2}})
+              :status "SUCCEEDED"}
+             (as-> input $
                (sw/offload-select-keys-to-s3 $ [:x :y] bucket-name)
                (hash-map :input $)
                (sw/start-execution!! :stepwise-integration-test $)
