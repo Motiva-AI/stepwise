5bd20b20734a967e92c8a5708d11c426b19beb93
Paul Lam <paul@quantisan.com>
2021-10-25 08:43:33 +0900

    remove basic-state-machine-test

--- test/stepwise/core_test.clj
+++ test/stepwise/core_test.clj
@@ -8,26 +8,6 @@
 (def state-machine-keyword-name :stepwise-integration-test)
 (def bucket-name "stepwise-integration-test")
 
-(deftest ^:integration basic-state-machine-test
-  (when (is (sw/ensure-state-machine state-machine-keyword-name
-                                     {:start-at :add
-                                      :states   {:add {:type     :task
-                                                       :resource :stepwise-integration-test/add
-                                                       :heartbeat-seconds 3
-                                                       :end      true}}}))
-
-    (let [workers (sw/start-workers!
-                    {:stepwise-integration-test/add (fn [{:keys [x y]}] (+ x y)) })]
-
-      (is (= {:output 2
-              :status "SUCCEEDED"}
-             (as-> {:x 1 :y 1} $
-               (sw/offload-select-keys-to-s3 $ [:x :y] bucket-name)
-               (hash-map :input $)
-               (sw/start-execution!! :stepwise-integration-test $)
-               (select-keys $ [:output :status]))))
-      (is (= #{:done} (sw/shutdown-workers workers))))))
-
 (deftest ^:integration manipulate-task-input-and-output-test
   (when (is (sw/ensure-state-machine state-machine-keyword-name
                                      {:start-at :add
