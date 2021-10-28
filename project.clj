f22f21f4eaf1ad9f27f190244f48030de1f74ec2
Paul Lam <paul@quantisan.com>
2021-10-28 14:20:35 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                                   [org.clojure/spec.alpha "0.2.194"]
                                   [pjstadig/humane-test-output "0.11.0"]
                                   [org.slf4j/slf4j-simple "1.7.32"]
-                                  [com.gfredericks/test.chuck "0.2.12"]
+                                  [com.gfredericks/test.chuck "0.2.13"]
                                   [circleci/bond "0.6.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
@@ -27,11 +27,11 @@
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/core.async "1.3.622"]
                  [metosin/sieppari "0.0.0-alpha13"]
-                 [com.amazonaws/aws-java-sdk-iam "1.12.90"]
-                 [com.amazonaws/aws-java-sdk-sts "1.12.90"]
-                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.90"]
+                 [com.amazonaws/aws-java-sdk-iam "1.12.97"]
+                 [com.amazonaws/aws-java-sdk-sts "1.12.97"]
+                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.97"]
                  [com.cognitect.aws/api "0.8.524"]
-                 [com.cognitect.aws/endpoints "1.1.12.88"]
+                 [com.cognitect.aws/endpoints "1.1.12.93"]
                  [com.cognitect.aws/s3 "814.2.991.0"]
                  [com.taoensso/nippy "3.1.1"]])
 
