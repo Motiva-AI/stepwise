58d76e09ee5c065ffdd361b8bbdb96fe5175534a
Paul Lam <paul@quantisan.com>
2022-10-27 06:56:08 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/Motiva-AI/stepwise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.1.0"]
-                                  [org.clojure/clojure "1.10.3"]
-                                  [org.clojure/spec.alpha "0.2.194"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]
+                                  [org.clojure/clojure "1.11.1"]
+                                  [org.clojure/spec.alpha "0.3.218"]
                                   [pjstadig/humane-test-output "0.11.0"]
-                                  [org.slf4j/slf4j-simple "1.7.32"]
+                                  [org.slf4j/slf4j-simple "2.0.3"]
                                   [com.gfredericks/test.chuck "0.2.13"]
                                   [circleci/bond "0.6.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
-                                  [org.clojure/test.check "1.1.0"]]
+                                  [org.clojure/test.check "1.1.1"]]
                    :injections   [(require 'pjstadig.humane-test-output)
                                   (pjstadig.humane-test-output/activate!)]
                    :main         stepwise.dev-repl}}
@@ -23,15 +23,15 @@
                                    :sign-releases false}}
 
   :dependencies [[uwcpdx/bean-dip "0.7.6"]
-                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/data.json "2.4.0"]
-                 [org.clojure/core.async "1.4.627"]
+                 [org.clojure/core.async "1.5.648"]
                  [metosin/sieppari "0.0.0-alpha13"]
-                 [com.amazonaws/aws-java-sdk-iam "1.12.110"]
-                 [com.amazonaws/aws-java-sdk-sts "1.12.110"]
-                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.110"]
-                 [com.cognitect.aws/api "0.8.524"]
-                 [com.cognitect.aws/endpoints "1.1.12.105"]
-                 [com.cognitect.aws/s3 "814.2.991.0"]
-                 [com.taoensso/nippy "3.1.1"]])
+                 [com.amazonaws/aws-java-sdk-iam "1.12.329"]
+                 [com.amazonaws/aws-java-sdk-sts "1.12.329"]
+                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.329"]
+                 [com.cognitect.aws/api "0.8.603"]
+                 [com.cognitect.aws/endpoints "1.1.12.321"]
+                 [com.cognitect.aws/s3 "822.2.1145.0"]
+                 [com.taoensso/nippy "3.2.0"]])
 
