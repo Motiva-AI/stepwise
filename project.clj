92a6905aade66e0cd5d749b203bc7f0db5c6f773
Paul Lam <paul@quantisan.com>
2023-07-19 09:16:58 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject ai.motiva/stepwise "0.9.0-SNAPSHOT"
+(defproject ai.motiva/stepwise "0.8.1-SNAPSHOT"
   :description "Clojure AWS Step Functions library"
   :url "https://github.com/Motiva-AI/stepwise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.4.4"]
                                   [org.clojure/clojure "1.11.1"]
                                   [org.clojure/spec.alpha "0.3.218"]
                                   [pjstadig/humane-test-output "0.11.0"]
-                                  [org.slf4j/slf4j-simple "2.0.3"]
-                                  [com.gfredericks/test.chuck "0.2.13"]
+                                  [org.slf4j/slf4j-simple "2.0.7"]
+                                  [com.gfredericks/test.chuck "0.2.14"]
                                   [circleci/bond "0.6.0"]
                                   ; TODO pending jdk 9 compat https://github.com/pallet/alembic/pull/16
                                   #_[alembic "0.3.2"]
@@ -27,11 +27,11 @@
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/core.async "1.6.673"]
                  [metosin/sieppari "0.0.0-alpha13"]
-                 [com.amazonaws/aws-java-sdk-iam "1.12.332"]
-                 [com.amazonaws/aws-java-sdk-sts "1.12.332"]
-                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.332"]
-                 [com.cognitect.aws/api "0.8.612"]
-                 [com.cognitect.aws/endpoints "1.1.12.321"]
-                 [com.cognitect.aws/s3 "822.2.1145.0"]
+                 [com.amazonaws/aws-java-sdk-iam "1.12.508"]
+                 [com.amazonaws/aws-java-sdk-sts "1.12.508"]
+                 [com.amazonaws/aws-java-sdk-stepfunctions "1.12.508"]
+                 [com.cognitect.aws/api "0.8.686"]
+                 [com.cognitect.aws/endpoints "1.1.12.504"]
+                 [com.cognitect.aws/s3 "848.2.1413.0"]
                  [com.taoensso/nippy "3.2.0"]])
 
